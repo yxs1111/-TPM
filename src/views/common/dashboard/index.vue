da06f71aa5719c35f99cd9e723211a611bb84ee0
“巧笑倩兮” <1240893912@qq.com>
2021-11-16 20:37:57 +0800

    首页样式V1.0

--- src/views/common/dashboard/index.vue
+++ src/views/common/dashboard/index.vue
@@ -1,11 +1,45 @@
 <!--
  * @Description: 
  * @Date: 2021-08-30 10:38:43
- * @LastEditTime: 2021-11-16 14:53:25
+ * @LastEditTime: 2021-11-16 20:32:05
 -->
 <template>
   <div class="dashboard-container">
-    首页
+    <div class="TopBar">
+      <div class="Topli">
+        <div>上月总金额</div>
+        <div class="TopliNumWrap">
+          <span class="unit">¥</span>
+          <span class="TopliNum">564,839.00</span>
+        </div>
+      </div>
+      <div class="Topli">
+        <div>上月总销量</div>
+        <div class="TopliNumWrap">
+          <span class="TopliNum">396.0</span>
+        </div>
+      </div>
+      <div class="Topli">
+        <div>上月新客达成率</div>
+        <div class="TopliNumWrap">
+          <span class="TopliNum">80%</span>
+        </div>
+      </div>
+      <div class="Topli">
+        <div>待审批任务</div>
+        <div class="TopliNumWrap">
+          <span class="TopliNum">483</span>
+        </div>
+      </div>
+    </div>
+    <div class="chartWrap">
+      <div class="chartli" id="SalesAmount"></div>
+      <div class="chartli" id="ActualSales"></div>
+    </div>
+    <div class="BottomBar">
+      <div class="MyToDo"></div>
+      <div class="Message MyToDo"></div>
+    </div>
   </div>
 </template>
 
@@ -14,21 +48,211 @@ import auth from '@/utils/auth'
 
 export default {
   name: 'Dashboard',
-  created() {
-
-  },
+  created() {},
   data() {
     return {
       avatar: auth.getAvatar(),
       name: auth.getName(),
+      SalesAmountChart: '',
+      ActualSalesChart: ''
+    }
+  },
+  mounted() {
+    this.createdEchart()
+    window.addEventListener('resize', () => {
+      this.SalesAmountChart.resize()
+      this.ActualSalesChart.resize()
+    })
+  },
+  watch: {
+    '$store.state.app.sidebar.opened'() {
+      setTimeout(() => {
+        this.SalesAmountChart.resize()
+        this.ActualSalesChart.resize()
+      }, 100)
     }
   },
   methods: {
-    
+    // 创建图表
+    createdEchart() {
+      var SalesAmountChart = echarts.init(
+        document.getElementById('SalesAmount')
+      )
+      SalesAmountChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        title: {
+          text: '每月促销金额',
+          textStyle: {
+            fontSize: 22,
+            color: '#333'
+          },
+          top:'4%',
+          left: "2%",
+        },
+        grid: {
+          top: '24%',
+          left: '5%',
+          right: '5%',
+          bottom: '10%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+          }
+        ]
+      })
+      this.SalesAmountChart = SalesAmountChart
+      var ActualSalesChart = echarts.init(
+        document.getElementById('ActualSales')
+      )
+      ActualSalesChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'line' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        title: {
+          text: '每月实际销量',
+          textStyle: {
+            fontSize: 22,
+            color: '#333',
+          },
+          top:'4%',
+          left: "2%",
+        },
+        grid: {
+          top: '24%',
+          left: '5%',
+          right: '5%',
+          bottom: '10%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+          }
+        ]
+      })
+      this.ActualSalesChart = ActualSalesChart
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.dashboard-container {
+  font-size: 16px;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  .TopBar {
+    width: 100%;
+    height: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .Topli {
+      // width: 361px;
+      height: 160px;
+      width: 24%;
+      // height: 100%;
+      border-radius: 20px;
+      padding: 36px 45px;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      color: #fafcfd;
+      box-shadow: -2px 0px 29px 0px rgba(57, 172, 253, 0.69);
+      font-size: 20px;
+      color: #fafcfd;
+      background: url('../../../assets/images/index/HomeBG (4).png') no-repeat;
+      // background-position: -30px -30px;
+      background-size: 100% 100%;
+      .TopliNumWrap {
+        .unit {
+          margin-right: 5px;
+        }
+        .TopliNum {
+          font-family: SourceHanSansCN-Regular;
+          font-size: 38px;
+          font-weight: 600;
+        }
+      }
+    }
+    & .Topli:nth-child(4n + 1) {
+      background-image: url('../../../assets/images/index/HomeBG (1).png');
+    }
+    & .Topli:nth-child(4n + 2) {
+      background-image: url('../../../assets/images/index/HomeBG (2).png');
+      box-shadow: -2px 0px 29px 0px rgba(250, 133, 102, 0.69);
+    }
+    & .Topli:nth-child(4n + 3) {
+      background-image: url('../../../assets/images/index/HomeBG (3).png');
+      box-shadow: -2px 0px 29px 0px rgba(58, 218, 202, 0.69);
+    }
+    & .Topli:nth-child(4n + 4) {
+      background-image: url('../../../assets/images/index/HomeBG (4).png');
+      box-shadow: -2px 0px 29px 0px rgba(255, 185, 65, 0.69);
+    }
+  }
+  .chartWrap {
+    width: 100%;
+    height: 310px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 32px 0;
+    .chartli {
+      width: 49%;
+      height: 310px;
+      background-color: #fff;
+      border-radius: 20px;
+      box-shadow: 0px 0px 20px 0px rgba(65, 146, 211, 0.04);
+    }
+  }
+  .BottomBar {
+    width: 100%;
+    height: 310px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 32px 0;
+    .MyToDo {
+      width: 65%;
+      height: 310px;
+      background-color: #fff;
+      border-radius: 20px;
+      box-shadow: 0px 0px 20px 0px rgba(65, 146, 211, 0.04);
+    }
+    .Message {
+      width: 33%;
+    }
+  }
+}
 </style>
