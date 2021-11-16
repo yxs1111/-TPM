a5dc999c76696ff036401662470ca25c0e6dc371
mayue <mayue@jbinfo.cn>
2021-11-16 08:32:08 +0800

    价格和促销

---
+++ src/views/master/modelIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- xxx html 内容  -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PriceModel',
+  data() {
+    return {}
+  }
+}
+</script>
