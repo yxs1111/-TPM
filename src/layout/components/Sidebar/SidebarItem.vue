fc860022277ab620fe525de422e1c37d2d2daac0
mayue <mayue@jbinfo.cn>
2021-12-06 19:08:28 +0800

    svg图片样式

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -17,16 +17,16 @@
             :title="generateTitle(onlyOneChild.meta.title)"
           />
         </el-menu-item>
-        
+
       </app-link>
       <!-- 首页自定义 -->
-        <div v-if="onlyOneChild.meta&&onlyOneChild.path === 'dashboard'" class="dashboardBar">
-          <div class="dashBoard" v-if="sidebar.opened" @click="goDashBoard">
-            <img src="../../../assets/images/clickIndex.png" alt="" class="clickIndex">
-            <span class="IndexText">首页</span>
-          </div>
-          <img src="../../../assets/images/toggle.png" alt="" class="toggleIcon"  @click.stop="toggleSideBar">
+      <div v-if="onlyOneChild.meta&&onlyOneChild.path === 'dashboard'" class="dashboardBar">
+        <div v-if="sidebar.opened" class="dashBoard" @click="goDashBoard">
+          <img src="../../../assets/images/clickIndex.png" alt="" class="clickIndex">
+          <span class="IndexText">首页</span>
         </div>
+        <img src="../../../assets/images/toggle.png" alt="" class="toggleIcon" @click.stop="toggleSideBar">
+      </div>
     </template>
 
     <el-submenu
@@ -49,24 +49,26 @@
         :item="child"
         :base-path="resolvePath(child.path)"
         class="nest-menu"
-      />
+      ><div style="background:red;width:10px;height:10px;"></div>
+      </sidebar-item>
+      
     </el-submenu>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import path from "path";
-import { generateTitle } from "@/utils/i18n";
-import { isExternal } from "@/utils/validate";
-import Item from "./Item";
-import AppLink from "./Link";
-import FixiOSBug from "./FixiOSBug";
+import path from 'path'
+import { generateTitle } from '@/utils/i18n'
+import { isExternal } from '@/utils/validate'
+import Item from './Item'
+import AppLink from './Link'
+import FixiOSBug from './FixiOSBug'
 import Hamburger from '@/components/Hamburger'
 
 export default {
-  name: "SidebarItem",
-  components: { Item, AppLink ,Hamburger},
+  name: 'SidebarItem',
+  components: { Item, AppLink, Hamburger },
   mixins: [FixiOSBug],
   props: {
     // route object
@@ -80,68 +82,68 @@ export default {
     },
     basePath: {
       type: String,
-      default: ""
+      default: ''
     }
   },
   data() {
     // To fix https://github.com/PanJiaChen/vue-admin-template/issues/237
     // TODO: refactor with render function
-    this.onlyOneChild = null;
+    this.onlyOneChild = null
     return {
-      
-    };
+
+    }
   },
   computed: {
     ...mapGetters([
       'sidebar'
-    ]),
+    ])
   },
   methods: {
     hasOneShowingChild(parent, children = []) {
       const showingChildren = children.filter(item => {
         if (item.hidden) {
-          return false;
+          return false
         } else {
           // Temp set(will be used if only has one showing child)
-          this.onlyOneChild = item;
-          return true;
+          this.onlyOneChild = item
+          return true
         }
-      });
+      })
 
       // When there is only one child router, the child router is displayed by default
       if (showingChildren.length === 1) {
-        return true;
+        return true
       }
 
       // Show parent if there are no child router to display
       if (showingChildren.length === 0) {
-        this.onlyOneChild = { ...parent, path: "", noShowingChildren: true };
-        return true;
+        this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
+        return true
       }
 
-      return false;
+      return false
     },
     resolvePath(routePath) {
       if (isExternal(routePath)) {
-        return routePath;
+        return routePath
       }
       if (isExternal(this.basePath)) {
-        return this.basePath;
+        return this.basePath
       }
-      return path.resolve(this.basePath, routePath);
+      return path.resolve(this.basePath, routePath)
     },
     toggleSideBar() {
-      this.$emit('changeSideBar',this.sidebar);
-      console.log(this.sidebar);
+      this.$emit('changeSideBar', this.sidebar)
+      console.log(this.sidebar)
       this.$store.dispatch('app/toggleSideBar')
     },
     goDashBoard() {
-      this.$router.push('/dashboard');
-      this.$store.dispatch('app/hideBreadcrumb') //进入首页隐藏面包屑
+      this.$router.push('/dashboard')
+      this.$store.dispatch('app/hideBreadcrumb') // 进入首页隐藏面包屑
     },
     generateTitle
   }
-};
+}
 </script>
 <style scoped lang="scss">
 @import "@/styles/variables.scss";
@@ -185,7 +187,7 @@ export default {
     .IndexText {
       letter-spacing: 6px;
       font-size: 16px;
-      
+
       font-family: Source Han Sans CN;
     }
   }
