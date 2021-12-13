860bd16335ed849d595d707d6a284ac8ef1fb4be
mayue <mayue@jbinfo.cn>
2021-12-13 09:22:22 +0800

    超时时间设置

--- vue.config.js
+++ vue.config.js
@@ -65,14 +65,15 @@ module.exports = {
       // target: `http://192.168.40.165:9012`,  //宋佳
       // target: `http://192.168.40.90:7777`,  //宗桂
       // target: `http://10.176.80.242:7777`, // 测试服务器
-        target: `http://192.168.50.156:7788`, // 张淼
+        // target: `http://192.168.50.156:7788`, // 张淼
         // target: `http://192.168.40.146:7777`, // 王宝兴
         // target: `http://192.168.50.25:7777`, // 樊鹏伟
-        // target: `https://uat-iinvest.rfc-friso.com:8080/prod-api`,  //测试服务器
+        target: `https://uat-iinvest.rfc-friso.com:8080/prod-api`,  //测试服务器
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
-        }
+        },
+        timeout: 3600 * 1000
       }
     }
   },
