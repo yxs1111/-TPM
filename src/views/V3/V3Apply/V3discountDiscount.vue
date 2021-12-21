bb4a00a0739eef20a9bd0f72f1bf03e502d11e01
mayue <mayue@jbinfo.cn>
2021-12-21 23:45:56 +0800

    V3修改

--- src/views/V3/V3Apply/V3discountDiscount.vue
+++ src/views/V3/V3Apply/V3discountDiscount.vue
@@ -550,8 +550,8 @@ export default {
               message: '上传失败，请重新上传。'
             })
           }
-          //清除input的value ,上传一样的
-        this.event.srcElement.value = '' // 置空
+          // 清除input的value ,上传一样的
+          this.event.srcElement.value = '' // 置空
         })
         .catch(() => {})
     },
@@ -583,6 +583,7 @@ export default {
               message: '上传失败，请重新上传。'
             })
           }
+          this.event.srcElement.value = ''
         })
         .catch(() => {})
     },
