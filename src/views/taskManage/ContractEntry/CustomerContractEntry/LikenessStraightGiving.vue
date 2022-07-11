68cc229a19eeda1e58d1aee2c6c47781e4b02155
wangwei <wangwei@jbinfo.cn>
2022-07-11 08:59:57 +0800

    提示语修改

--- src/views/taskManage/ContractEntry/CustomerContractEntry/LikenessStraightGiving.vue
+++ src/views/taskManage/ContractEntry/CustomerContractEntry/LikenessStraightGiving.vue
@@ -1,7 +1,7 @@
 <!--
  * @Description: 
  * @Date: 2021-11-16 14:01:16
- * @LastEditTime: 2022-07-08 15:26:39
+ * @LastEditTime: 2022-07-11 08:56:57
 -->
 <template>
   <div class="MainContent">
@@ -1303,7 +1303,7 @@ export default {
         date: row.expireDate,
       }).then((res) => {
         if (res.code === 1000) {
-          this.$message.success('终止成功')
+          this.$message.success('调整成功')
           this.popoverCancel(row.id)
           this.getTableData()
         }
--- src/views/taskManage/ContractEntry/CustomerContractEntry/UnStraightGiving.vue
+++ src/views/taskManage/ContractEntry/CustomerContractEntry/UnStraightGiving.vue
@@ -1,7 +1,7 @@
 <!--
  * @Description: 
  * @Date: 2021-11-16 14:01:16
- * @LastEditTime: 2022-07-08 15:18:53
+ * @LastEditTime: 2022-07-11 08:56:37
 -->
 <template>
   <div class="MainContent">
@@ -1304,7 +1304,7 @@ export default {
         date: row.expireDate,
       }).then((res) => {
         if (res.code === 1000) {
-          this.$message.success('终止成功')
+          this.$message.success('调整成功')
           this.popoverCancel(row.id)
           this.getTableData()
         }
