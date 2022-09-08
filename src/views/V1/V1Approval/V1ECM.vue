eb3998e79905af4a4877e5dfbd2fe6300cb47edb
wangwei <wangwei@jbinfo.cn>
2022-09-08 10:01:54 +0800

    v1字段绑定更正

--- src/views/V1/V1Approval/V1ECM.vue
+++ src/views/V1/V1Approval/V1ECM.vue
@@ -31,7 +31,7 @@
             <el-option v-for="(item) in channelArr"
                        :key="item.channelCsName"
                        :label="item.channelCsName"
-                       :value="item.channelCsCode" />
+                       :value="item.channelCode" />
           </el-select>
         </div>
         <div class="Selectli">
@@ -43,7 +43,7 @@
             <el-option v-for="(item, index) in customerArr"
                        :key="index"
                        :label="item.customerCsName"
-                       :value="item.customerCsCode" />
+                       :value="item.customerCode" />
           </el-select>
         </div>
         <!-- <div class="Selectli">
