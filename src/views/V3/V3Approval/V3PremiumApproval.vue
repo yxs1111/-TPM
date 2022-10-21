555bf7ba1a73688939741b4276a2422895390b1e
yaoxiansheng@jbinfo.cn <1798980103@qq.com>
2022-10-21 13:38:45 +0800

    Premiun 中版本号跟更换
    (cherry picked from commit a26387a2348562fd4195155a77eca6356bbd1287)

--- src/views/V3/V3Approval/V3PremiumApproval.vue
+++ src/views/V3/V3Approval/V3PremiumApproval.vue
@@ -833,7 +833,7 @@ export default {
         .then((res) => {
           if (res.code === 1000) {
             if (
-              res.data.version === 'V3' &&
+              res.data.version === 'Premium-V3' &&
               res.data.assignee.indexOf(this.usernameLocal) != -1 &&
               this.tableData[0].isSubmit
             ) {
