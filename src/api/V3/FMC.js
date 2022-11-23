92717a1b8579b3db7ee1db6fa83328d541085c7c
yaoxiansheng@jbinfo.cn <1798980103@qq.com>
2022-11-23 15:05:51 +0800

    FMC  V3Ec几口对接

--- src/api/V3/FMC.js
+++ src/api/V3/FMC.js
@@ -12,7 +12,11 @@ export default {
   getPage(params) {
     return requestApi.request_get(this.url+'/getV3Page', params)
   },
-  //导出V2
+  //申请 EC分页查询
+  getPageEC(params) {
+    return requestApi.request_get('/cityplan/investCpVThreeEFmc/getVThreePage', params)
+  },
+  //导出V3
   exportV3(params) {
     return request({
       url: this.url+'/exportV3',
@@ -21,9 +25,22 @@ export default {
       responseType: 'blob'
     })
   },
+  // EC导出V3
+  exportECV3(params) {
+    return request({
+      url: '/cityplan/investCpVThreeEFmc/exportVThree',
+      method: 'get',
+      params: params,
+      responseType: 'blob'
+    })
+  },
   //保存
   saveV3Data(params) {
     return requestApi.request_put(this.url+'/saveV3Data', params)
+  },
+  // EC保存
+  saveV3DataEC(params) {
+    return requestApi.request_post('/cityplan/investCpVThreeEFmc/saveVThree', params)
   },
   //下载模板
   downloadTemplate(params) {
@@ -34,13 +51,30 @@ export default {
       responseType: 'blob'
     })
   },
+  // EC下载模板
+  downloadTemplateEC(params) {
+    return request({
+      url: '/cityplan/investCpVThreeEFmc/downloadTemplate',
+      method: 'get',
+      params: params,
+      responseType: 'blob'
+    })
+  },
   //导入
   import(params) {
     return requestApi.request_post(this.url+'/importV3', params)
   },
+  // EC导入
+  importEC(params) {
+    return requestApi.request_post('/cityplan/investCpVThreeEFmc/importVThree', params)
+  },
   //异常项校验
   exceptionCheck(params) {
     return requestApi.request_post(this.url+'/exceptionCheckV3', params)
+  },
+  // EC异常项校验
+  exceptionCheckEC(params) {
+    return requestApi.request_post('/cityplan/investCpVThreeEFmc/exceptionCheck', params)
   },
   //导出错误信息
   exportV3Error(params) {
@@ -51,12 +85,29 @@ export default {
       responseType: 'blob'
     })
   },
+  // EC导出错误信息
+  exportV3ErrorEC(params) {
+    return request({
+      url: '/cityplan/investCpVThreeEFmc/exportError',
+      method: 'get',
+      params: params,
+      responseType: 'blob'
+    })
+  },
   //提交
   approve(params) {
     return requestApi.request_post(this.url+'/v3Approve', params)
   },
+  // EC提交
+  approveEC(params) {
+    return requestApi.request_post('/cityplan/investCpVThreeEFmc/approveVThree', params)
+  },
   //获取审批
   getApproveList(params) {
     return requestApi.request_get(this.url+'/getV3ApproveList', params)
+  },
+  // EC获取审批
+  getApproveListEC(params) {
+    return requestApi.request_get('/cityplan/investCpVThreeEFmc/getVThreeApprovePage', params)
   },
 }
