7a4c76cbc755346ff8f00f55663c5125c200f0a2
yaoxiansheng@jbinfo.cn <1798980103@qq.com>
2023-02-08 14:40:41 +0800

    V2申请中version问题更改完毕

--- src/views/V2/V2Apply/V2Others.vue
+++ src/views/V2/V2Apply/V2Others.vue
@@ -947,7 +947,7 @@ export default {
         .then((res) => {
           if (res.code === 1000) {
             if (
-              res.data.version == 'Others-V2' && res.data.activityName.includes('调整') &&
+              res.data.version.includes('V2') && res.data.activityName.includes('调整') &&
               res.data.assignee.indexOf(this.usernameLocal) != -1
             ) {
               //本人可以提交
--- src/views/V2/V2Apply/V2RoadShow.vue
+++ src/views/V2/V2Apply/V2RoadShow.vue
@@ -1257,7 +1257,7 @@ export default {
         .then((res) => {
           if (res.code === 1000) {
             if (
-              res.data.version === 'Roadshow-V2' &&
+              res.data.version.includes('V2') &&
               res.data.assignee.indexOf(this.usernameLocal) != -1
             ) {
               // 本人可以提交
